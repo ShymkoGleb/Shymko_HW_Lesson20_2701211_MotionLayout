8dc84f78381e88a7ba8d94eec9bb5caaeb5c84e3
ShymkoGleb <71500261+ShymkoGleb@users.noreply.github.com>
2021-02-04 23:03:24 +0200

    Done added dimension in res

--- app/src/main/java/com/example/shymko_hw_lesson20_2701211_motionlayout/MainActivity.kt
+++ app/src/main/java/com/example/shymko_hw_lesson20_2701211_motionlayout/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.shymko_hw_lesson20_2701211_motionlayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+      //  setContentView(R.layout.activity_main)
     }
 }
